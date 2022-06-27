842161e299476276436deaf894387dca029dc00f
willco213 <87068102+willco123@users.noreply.github.com>
2022-06-27 15:02:22 +0100

    challenge 6 prog

--- CryptoPalsSetOne.py
+++ CryptoPalsSetOne.py
@@ -1,9 +1,12 @@
 from base64 import b64encode
 from base64 import b64decode
+from pprint import pprint
 from gmpy2 import popcount
 from itertools import combinations
 from itertools import pairwise
-
+from functools import cache
+import timeit
+import collections
 
 
 
@@ -96,38 +99,80 @@ def breakRepeatingKeyXOR(fileObj):#input is a b64 string
         b64String = f.read()
     hexString = b64decode(b64String).hex()
     byteString = bytes.fromhex(hexString)
-    GuessKeyLength(byteString)
+    #CombKeyEditDistVerbose(byteString)
+    #GuessKeyLengthWeightedAverage(byteString)
+    print(GuessKeyLength(byteString, 4))
+    #print(GuessKeyLengthVerbose(byteString, 8))
+    print(GuessKeyLengthWeightedAverage(byteString))
 
-def MostLikelyKeyLengths(byteString):
-    for i in range():
-        pass
-
-def GuessKeyLength(byteString):
-    dict = {}
-    combDict = {}
+def GuessKeyLength(byteString, numberOfBlocks):#Fixed number of blocks for this one
     pairDict = {}
+    maxKeyLength = len(byteString)//numberOfBlocks
 
-    numberOfBlocks = 4
-    maxKeyLength = 40
     if numberOfBlocks%2 != 0:
         raise ValueError('Number of Blocks must be even.')
     
     if numberOfBlocks * maxKeyLength > len(byteString):
         raise ValueError('maxKeyLength times the Number of Blocks cannot exceed the number of bytes in byteString, len(byteSttring is:', len(byteString))
 
-    if maxKeyLength < 2:
-        raise ValueError('Max Key Length must exceed 1.')
-
-    for keyLength in range(1, maxKeyLength, 1):
+    for keyLength in range(1, maxKeyLength+1, 1):
         byteBlocks = [byteString[i:i+keyLength] for i in range(0, numberOfBlocks*keyLength, keyLength)]
-        dict[keyLength] = PairKeyEditDist(byteBlocks, keyLength)
-        combDict[keyLength] = CombKeyEditDist(byteBlocks, keyLength)
-        pairDict[keyLength] = OverLappingPairKeyEditDist(byteBlocks, keyLength)
-        
-    print(min(dict.values()), min(dict, key=dict.get))
-    print(min(combDict.values()), min(combDict, key=dict.get))
-    print(min(pairDict.values()), min(pairDict, key=dict.get))
+        pairDict[keyLength, numberOfBlocks] = PairKeyEditDist(byteBlocks, keyLength)
 
+    pprint(pairDict)
+    return (min(pairDict.values()), min(pairDict, key=pairDict.get))
+
+def GuessKeyLengthVerbose(byteString, numberOfBlocks):#Fixed number of blocks for this one, all methods of combinations
+    pairDict = {}
+    combDict = {}
+    overLappingPairDict = {}
+    maxKeyLength = len(byteString)//numberOfBlocks
+
+    if numberOfBlocks%2 != 0:
+        raise ValueError('Number of Blocks must be even.')
+    
+    if numberOfBlocks * maxKeyLength > len(byteString):
+        raise ValueError('maxKeyLength times the Number of Blocks cannot exceed the number of bytes in byteString, len(byteSttring is:', len(byteString))
+
+    for keyLength in range(1, maxKeyLength+1, 1):
+        byteBlocks = [byteString[i:i+keyLength] for i in range(0, numberOfBlocks*keyLength, keyLength)]
+        pairDict[keyLength, numberOfBlocks] = PairKeyEditDist(byteBlocks, keyLength)
+        combDict[keyLength, numberOfBlocks] = CombKeyEditDist(byteBlocks, keyLength)
+        overLappingPairDict[keyLength, numberOfBlocks] = OverLappingPairKeyEditDist(byteBlocks, keyLength)
+
+    print(min(pairDict.values()), min(pairDict, key=pairDict.get))
+    print(min(combDict.values()), min(combDict, key=combDict.get))
+    print(min(pairDict.values()), min(pairDict, key=pairDict.get))
+
+def GuessKeyLengthWeightedAverage(byteString):#This function will average the hamming dist with respect for keylength for a variable number of blocks to gather a weighted guess of the correct keylength
+    weightedPairDict = {}
+
+    maxKeyLength = len(byteString)//2
+
+    for keyLength in range(1, maxKeyLength+1, 1):
+        maxBlocks = len(byteString)//keyLength
+        byteBlocks = [byteString[i:i+keyLength] for i in range(0, maxBlocks*keyLength, keyLength)]
+        PairKeyEditDistWeighted(byteBlocks, keyLength, weightedPairDict)
+
+    sumKeyLengths = {k[0]:0 for k in weightedPairDict.keys()}
+    counts = dict(collections.Counter(k[0] for k in weightedPairDict))
+
+    for key in weightedPairDict:
+        sumKeyLengths[key[0]] = sumKeyLengths[key[0]] + (weightedPairDict[key]/counts[key[0]])
+
+    print(min(sumKeyLengths.values()), min(sumKeyLengths, key=sumKeyLengths.get))
+    return (min(weightedPairDict.values()), min(weightedPairDict, key=weightedPairDict.get))
+    
+def PairKeyEditDistWeighted(byteBlocks, keyLength, weightedPairDict):
+    pairDist = 0
+    numberOfPairs = 0
+
+    for i in range(0,len(byteBlocks)-1,2):#-1 here stops odd numbers going to function
+        pairDist += hammingDistance(byteBlocks[i], byteBlocks[i+1])/keyLength
+        numberOfPairs +=1
+        weightedPairDict[keyLength, i+2] =  pairDist/numberOfPairs
+    return weightedPairDict
+    #return sum(pairDict.values())/len(pairDict)
 
 def PairKeyEditDist(byteBlocks, keyLength):#Needs an even number of blocks to run
     dist = 0
@@ -156,7 +201,7 @@ def OverLappingPairKeyEditDist(byteBlocks, keyLength):#overlapping pairs
     return dist/numberOfPairs
         
     
-
+@cache
 def hammingDistance(bytesOne, bytesTwo):
 
     dist = 0
@@ -164,6 +209,17 @@ def hammingDistance(bytesOne, bytesTwo):
         val = i^o
         dist += popcount(val)
     return dist
+
+def testDict():
+    myDict = {}
+    myDict['a'] = 2
+    myDict = addDict(myDict)
+    print(myDict)
+
+def addDict(myDict):
+    myDict['a'] = 1
+    #print(myDict)
+    return myDict
 
 if __name__ == '__main__':
 
@@ -177,9 +233,66 @@ if __name__ == '__main__':
     stringOne = "this is a test"
     stringTwo = "wokka wokka!!!"
     #print(hammingDistance(stringOne, stringTwo))
-    breakRepeatingKeyXOR('B64RepeatingXOREncrypted.txt')
-    ##Re-write functions that the program re-uses to work on bytes
-    ##Convert ascii chars to byytes (already done that somewhere)
-    ##Create
 
-    min
+    breakRepeatingKeyXOR('B64RepeatingXOREncrypted.txt')
+    '''with open('B64RepeatingXOREncrypted.txt') as f:
+        b64String = f.read()
+    hexString = b64decode(b64String).hex()
+    byteString = bytes.fromhex(hexString)
+    print(timeit.timeit('CombKeyEditDistVerbose(byteString)', globals=globals(), setup = 'from __main__ import byteString', number = 10)/10)
+    print(timeit.timeit('CombKeyEditDist(byteString)', globals=globals(), setup = 'from __main__ import byteString', number = 10)/10)'''
+    
+    '''myDict = {}
+    
+    myDict['a', 'a'] = 1
+    myDict['a', 'b'] = 2
+    myDict['a', 'c'] = 3
+
+    myDict['b', 'a'] = 4
+    myDict['b', 'b'] = 5
+    myDict['b', 'c'] = 6
+    
+    for i in myDict.keys(): 
+        print(i[0])
+    
+    for i in myDict:
+        print('hi')
+        print(myDict[i])
+
+    sumKeyLengths = {k[0]:0 for k in myDict.keys()}
+    otherDict = {}
+
+    for k in myDict.keys():
+        otherDict[k[0]] = 0
+        #otherDict[k[0]] += 1
+        #print(otherDict(k[0]))
+
+    print(otherDict.keys())
+    print(sumKeyLengths.keys())
+    print(otherDict)
+
+
+    print(sumKeyLengths)
+
+    counts = dict(collections.Counter(k[0] for k in myDict))
+    print(type(counts['a']))
+
+    for key in myDict:
+        print(key)
+        sumKeyLengths[key[0]] = sumKeyLengths[key[0]] + (myDict[key]/counts[key[0]])
+    print(sumKeyLengths)
+    
+    for key in sumKeyLengths:
+        sumKeyLengths[key[0]] = sumKeyLengths[key[0]]/counts[key[0]]
+    print(sumKeyLengths)'''
+
+
+
+
+
+
+
+
+
+
+    
